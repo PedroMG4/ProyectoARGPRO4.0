4332d9d3c4208f89cb1e771e48075bb967ffe7ea
PedroMG4 <pedrogaray24@gmail.com>
2023-02-20 13:29:07 -0300

    Primer Commit

---
+++ JS/comportamientoContacto.js
@@ -0,0 +1,12 @@
+document.getElementById("Habilidades").addEventListener('click', function() {
+    window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Habilidades.html');
+})
+document.getElementById("Estudios").addEventListener('click', function() {
+    window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Estudios.html');
+})
+document.getElementById("Experiencia").addEventListener('click', function() {
+    window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Experiencia.html');
+})
+document.getElementById("BotonInicio").addEventListener('click', function() {
+    window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/CV Inicio.html');
+})
---
+++ JS/comportamientoInicio.js
@@ -0,0 +1,30 @@
+/**
+document.getElementById("Habilidades").addEventListener('click', function() {
+    window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Habilidades.html');
+})
+document.getElementById("Estudios").addEventListener('click', function() {
+    window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Estudios.html');
+})
+document.getElementById("Experiencia").addEventListener('click', function() {
+    window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Experiencia.html');
+})
+document.getElementById("Contacto").addEventListener('click', function() {
+    window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Contacto.html');
+})
+document.getElementById("BotonInicio").addEventListener('click', function() {
+    window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/CV Inicio.html');
+})
+*/
+
+    document.getElementById("Habilidades").addEventListener('click', function() {
+        window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Habilidades.html');
+    })
+    document.getElementById("Estudios").addEventListener('click', function() {
+        window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Estudios.html');
+    })
+    document.getElementById("Experiencia").addEventListener('click', function() {
+        window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Experiencia.html');
+    })
+    document.getElementById("Contacto").addEventListener('click', function() {
+        window.location.replace('C:/Users/Pedro/Desktop/JS/ProyectoCV/HTML/Contacto.html');
+    })
